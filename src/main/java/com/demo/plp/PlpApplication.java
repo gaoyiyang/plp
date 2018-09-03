c7d6a213b5e5439288f200963739cd4f2cefc9a3
GaoYiyang <370625414@qq.com>
2018-09-03 15:33:27 +0800

    1

---
+++ src/main/java/com/demo/plp/PlpApplication.java
@@ -0,0 +1,12 @@
+package com.demo.plp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlpApplication.class, args);
+	}
+}

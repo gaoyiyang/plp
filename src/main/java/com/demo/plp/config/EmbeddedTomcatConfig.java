788caf2de92bf1302df613d1e008835565fe6639
GaoYiyang <370625414@qq.com>
2018-09-04 00:48:10 +0800

    添加登陆页index.html

---
+++ src/main/java/com/demo/plp/config/EmbeddedTomcatConfig.java
@@ -0,0 +1,22 @@
+package com.demo.plp.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.stereotype.Component;
+@Component
+public class EmbeddedTomcatConfig implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+	@Override
+	public void customize(ConfigurableServletWebServerFactory factory) {
+		((TomcatServletWebServerFactory) factory).addConnectorCustomizers(new TomcatConnectorCustomizer() {
+			@Override
+			public void customize(Connector connector) {
+				Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+				protocol.setPort(80);
+			}
+		});
+	}
+}
